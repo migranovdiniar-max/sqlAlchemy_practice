bf09e09337ac6e38c19d3d494cd563b89c3182a3
Dinyar <migranovdiniar@gmail.com>
2025-11-12 06:39:09 +0300

    lots of stuff

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
 from sqlalchemy.orm import sessionmaker, Mapped, mapped_column
-from models import User, engine, Address
+from models import User_3, engine, Address_2, Node
 import random
 from sqlalchemy import or_, and_, not_, func
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-user1 = User(name='John Doe', age=52)
-user2 = User(name='Jane Smith', age=34)
+# --------------------------------------------------------------------------------------------------------------------
+# user1 = User(name='John Doe', age=52)
+# user2 = User(name='Jane Smith', age=34)
 
-# Creating addresses
-address1 = Address(
-    city='New York', state='NY', zip_code='10001'
-)
-address2 = Address(
-    city='Los Angeles', state='CA', zip_code='90001'
-)
-address3 = Address(
-    city='Chicago', state='IL', zip_code='60601'
-)
+# # Creating addresses
+# address1 = Address(
+#     city='New York', state='NY', zip_code='10001'
+# )
+# address2 = Address(
+#     city='Los Angeles', state='CA', zip_code='90001'
+# )
+# address3 = Address(
+#     city='Chicago', state='IL', zip_code='60601'
+# )
 
-user1.addresses.extend([address1, address2])
-user2.addresses.append(address3)
+# user1.addresses.extend([address1, address2])
+# user2.addresses.append(address3)
 
-session.add(user1)
-session.add(user2)
-session.commit()
+# session.add(user1)
+# session.add(user2)
+# session.commit()
 
-print(user1.addresses)
-print(user2.addresses)
-print(f"{address1.user = }")
+# print(user1.addresses)
+# print(user2.addresses)
+# print(f"{address1.user = }")
+# --------------------------------------------------------------------------------------------------------------------
+
+
 
 # users = session.query(User.age, func.count(User.id)).group_by(User.age).all()
 
@@ -62,4 +66,30 @@ print(f"{address1.user = }")
 
 # for user in users:
 #     print(user.age, user.name)
+# --------------------------------------------------------------------------------------------------------------------
 
+
+# new_user = User_3(name='John Doe')
+# new_address = Address_2(email='johndoe@gmail.com', user=new_user)
+# session.add(new_user)
+# session.add(new_address)
+
+# session.commit()
+# --------------------------------------------------------------------
+
+
+# node1 = Node(value=1)
+# node2 = Node(value=2)
+# node3 = Node(value=3)
+
+# node1.next_node = node2
+# node2.next_node = node3
+# node3.next_node = node1
+
+# session.add_all([node1, node2, node3])
+# session.commit()
+
+# print(node1)
+# print(node2)
+# print(node3)
+# --------------------------------------------------------------------------------------------
--- models.py
+++ models.py
@@ -38,25 +38,64 @@ class User(BaseModel):
     addresses: Mapped[list["Address"]] = relationship()
 
 
-class FollowingAssociation(BaseModel):
-    __tablename__ = "following_association"
+# --------------------------------------------------------------------------------------------------------------------
+# class FollowingAssociation(BaseModel):
+#     __tablename__ = "following_association"
 
-    user_2_id = Column(Integer, ForeignKey("user_2.id"))
-    following_id = Column(Integer, ForeignKey("user_2.id"))
+#     user_2_id = Column(Integer, ForeignKey("user_2.id"))
+#     following_id = Column(Integer, ForeignKey("user_2.id"))
 
 
-class User_2(BaseModel):
-    __tablename__ = 'users_2'
+# class User_2(BaseModel):
+#     __tablename__ = 'users_2'
 
-    username = Column(String)
+#     username = Column(String)
 
-    following = relationship('User', secondary="following_association", 
-                             primaryjoin=("FollowingAssociation.user_2.id==User_2.id"),
-                             secondaryjoin=("FollowingAssociation.following_id==User_2.id"),
-                             )
+#     following = relationship('User', secondary="following_association", 
+#                              primaryjoin=("FollowingAssociation.user_2.id==User_2.id"),
+#                              secondaryjoin=("FollowingAssociation.following_id==User_2.id"),
+#                              )
+
+#     def __repr__(self):
+#         return f"{self.id}, {self.username}"
+# --------------------------------------------------------------------------------------------------------------------
+
+class User_3(BaseModel):
+    __tablename__ = "users_3"
+
+    name = Column(String)
+    address = relationship("Address_2", back_populates="user", uselist=False)
+
+
+class Address_2(BaseModel):
+    __tablename__ = "addresses_2"
+
+    email = Column(String, unique=True)
+    user_id = Column(Integer, ForeignKey("users_3.id"))
+    user = relationship("User_3", back_populates="address")
+
+
+class NodeAssociation(BaseModel):
+    __tablename__ = 'node_association'
+
+    current_node_id = Column(Integer, ForeignKey('nodes.id'))
+    next_node_id = Column(Integer, ForeignKey("nodes.id"))
+
+class Node(BaseModel):
+    __tablename__ = 'nodes'
+
+    value = Column(Integer, nullable=False)
+    node_id = Column(Integer, ForeignKey("nodes.id"))
+    next_node = relationship(
+        "Node",
+        secondary='node_association',
+        primaryjoin="NodeAssociation.current_node_id==Node.id",
+        secondaryjoin="NodeAssociation.next_node_id==Node.id",
+        uselist=False,
+    )
 
     def __repr__(self):
-        return f"{self.id}, {self.username}"
+        return f"{self.value}, {self.next_node}"
 
 
 Base.metadata.create_all(engine)
